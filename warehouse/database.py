080b02931baa5bc880770b7cc0e49dee9d8df3d2
Kotletta-TT <assimilation13cd@gmail.com>
2020-11-02 20:07:59 +0300

    add docker and add readme

--- warehouse/database.py
+++ warehouse/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@localhost/secreate_test'
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:postgres@db/test_task'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
